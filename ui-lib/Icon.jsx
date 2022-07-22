2d6b9628cb0dc5009ab3c4eb341fd2cb94a1cc18
Fikri Firdaus <fikri.firdaus26@gmail.com>
2022-07-22 14:58:30 +0700

    update

--- ui-lib/Icon.jsx
+++ ui-lib/Icon.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { ReactComponent as DotsXIcon } from "https://raw.githubusercontent.com/Fikrifrds/ui-lib-dev/main/public/assets/icons/solid/dots-horizontal.svg";
+import { ReactComponent as DotsXIcon } from "./assets/icons/solid/dots-horizontal.svg";
 
 const Icon = ({
   name = "archive",
@@ -8,34 +8,7 @@ const Icon = ({
   type = "solid",
   ...rest
 }) => {
-  const ImportedIconRef = useRef(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const importIcon = async () => {
-      try {
-        const { ReactComponent: namedImport } = await import(
-          `https://raw.githubusercontent.com/Fikrifrds/ui-lib-dev/main/public/assets/icons/${type}/${name}.svg`
-        );
-        ImportedIconRef.current = namedImport;
-      } catch (err) {
-        throw err;
-      } finally {
-        setLoading(false);
-      }
-    };
-    importIcon();
-  }, [name]);
-
-  if (!loading && ImportedIconRef.current) {
-    const { current: ImportedIcon } = ImportedIconRef;
-    return <ImportedIcon width={size} height={size} fill={fill} {...rest} />;
-  } else {
-    return <DotsXIcon width={size} height={size} fill={fill} {...rest} />;
-  }
-
-  return null;
+  return <DotsXIcon width={size} height={size} fill={fill} {...rest} />;
 };
 
 export default Icon;
